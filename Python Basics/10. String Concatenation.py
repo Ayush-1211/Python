db907ef5aaf699a8656b8650543d37240c44e341
Ayush-1211 <63777803+Ayush-1211@users.noreply.github.com>
2021-06-28 19:52:59 +0530

    Python Basics

---
+++ Python Basics/10. String Concatenation.py
@@ -0,0 +1,8 @@
+print('Method 1::')
+print('Ayush' + ' Prajapati',"\n")
+
+print('Method 2::')
+first_name = 'Ayush'
+last_name = 'Prajapati'
+full_name = first_name + ' ' + last_name
+print(full_name)
