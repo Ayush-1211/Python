b72f173ad6b33f3d05cb546a034a2a9fdc3dd233
Ayush-1211 <63777803+Ayush-1211@users.noreply.github.com>
2021-06-28 19:52:30 +0530

    Python Basics

---
+++ Python Basics/7. Augmented Assignment Operator.py
@@ -0,0 +1,9 @@
+print('Method 1::')
+Value = 10
+Value = Value + 10
+print(Value)
+
+print('Method 2')
+Value = 30
+Value += 20     #Augmented Assignment Operator ( +=, -=, *=, /=, %= )
+print(Value)
---
+++ Python Basics/9. Strings.py
@@ -0,0 +1,14 @@
+print(type('Ayush Prajapati'),"\n")
+
+print('Single Quotation::')
+name = 'Ayush'
+print(name)
+print('Double Quotation::')
+name = "Prajapati"
+print(name,"\n")             #Single and Double quotation both works same
+
+print('Long String::')
+long_string = '''Hello
+I am Ayush Prajapati
+Welcome'''
+print(long_string)
