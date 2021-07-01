03572736cce8bcb9ad13e09ef58bd094b481a07b
Ayush-1211 <63777803+Ayush-1211@users.noreply.github.com>
2021-07-01 16:44:07 +0530

    Python Basics

---
+++ Python Basics/14. String Indexes.py
@@ -0,0 +1,32 @@
+print('Example 1:')
+name = 'Ayush'
+print(name[2])
+
+print('Example 2:')
+number = '01234567'
+print(number[1])
+
+print('Example 3:')
+print(number[0:4])      # [start:stop]
+
+print('Example 4:')
+print(number[0:8])
+
+print('Example 5:')
+print(number[0:8:2])    # [start:stop:stepover]
+
+print('Example 6:')
+print(number[1:])
+
+print('Example 7:')
+print(number[:5])
+
+print('Example 8:')
+print(number[::2])
+
+print('Example 9:')
+print(number[-1])
+
+print('Example 10:')
+print(number[::-1])
+print(number[::-2])
