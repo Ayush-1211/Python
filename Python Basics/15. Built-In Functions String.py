3a0c1eddadc98f6a0fe47d2a9bb59fdf3c0f98bc
Ayush-1211 <63777803+Ayush-1211@users.noreply.github.com>
2021-07-01 16:44:37 +0530

    Python Basics

---
+++ Python Basics/15. Built-In Functions String.py
@@ -0,0 +1,5 @@
+text = 'hello ayush prajapati'
+print(text.capitalize())    # Capitalize only First Character of text
+print(text.upper())
+print(text.replace('hello', 'hii'))
+print(text.find('ayush'))   # return first index number of that particular word
