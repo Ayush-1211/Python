de330c304616c73934d1ba3a6008976df3adad01
Ayush-1211 <63777803+Ayush-1211@users.noreply.github.com>
2021-07-11 18:14:46 +0530

    Python Basics

---
+++ Python Basics/29. Dictionary Key.py
@@ -0,0 +1,10 @@
+#Dictionary Key has to be unique and immutable
+
+dictionary = {
+    123 : [1,2,3],
+    True : 'Ayush',
+    'Hii' : 'Hello'
+}
+print(dictionary[123])
+print(dictionary[True])
+print(dictionary['Hii'])
