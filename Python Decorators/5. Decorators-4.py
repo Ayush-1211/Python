57693eba07ac203148700ac281b4405f53352cff
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-07-22 15:40:19 +0530

    Python Decorators

---
+++ Python Decorators/5. Decorators-4.py
@@ -0,0 +1,16 @@
+from time import time
+
+def performance(fn):
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        result = fn(*args,**kwargs)
+        t2 = time()
+        print(f'It took {t2-t1} s')
+        return result
+    return wrapper
+
+@performance
+def long_time():
+    for i in range(100000):
+        i * 5
+long_time()    
