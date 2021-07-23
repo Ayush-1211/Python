b3b1df8dddadcdd99716548bdbecec8f5c7d7488
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-07-23 14:12:49 +0530

    Error Handling

---
+++ Error Handling/1. Error Handling.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        age = int(input('Enter your age: '))
+        10/age
+    except ValueError:
+        print('Please enter a number!!!')
+    except ZeroDivisionError:
+        print('Please enter a number higher than 0!!!')
+    else:
+        print('Thank you!!!')
+        break
